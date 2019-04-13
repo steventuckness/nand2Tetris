81a8630fb321474a8f3a042fcbf5bf50118e2df9
Steven Tuckness <stuckne1@gmail.com>
2019-04-13 15:56:36 -0500

    fix bugs

--- projects/06/assembler/assemblerDriver.ts
+++ projects/06/assembler/assemblerDriver.ts
@@ -9,34 +9,31 @@ main();
 function main(): void { 
     var file = process.argv[2];
     var data = '';
-    
+
     const assemblyFileParser = new AssemblyFileParser(file);
     const assembler = new Asssembler();
  
     while(assemblyFileParser.hasMoreCommands()) {
         assemblyFileParser.advance();
         if (!assemblyFileParser.line) {
-            return;
+            break;
         }
 
         if (assemblyFileParser.commandType() === 'A_COMMAND') {
-            data += assemblyFileParser.symbol();
-            console.log(assemblyFileParser.symbol() + "\n");
+            data += assemblyFileParser.symbol() + "\n";
+            console.log(assemblyFileParser.symbol());
         } else if (assemblyFileParser.commandType() === 'C_COMMAND') {
             let start = '111';
             let jump = assembler.jump(assemblyFileParser.jump());    
             let comp = assembler.comp(assemblyFileParser.comp());
             let dest = assembler.dest(assemblyFileParser.dest());
             data += start + comp + dest + jump + "\n";
-            console.log(start + comp + dest + jump + "\n");
+            console.log(start + comp + dest + jump);
         }        
     } // end while
-
-    data  = "monkey";
-
-    console.log('about to start writing file');
+ 
     var fs = require("fs");
-    fs.writeFile(assemblyFileParser.newFile, data, function (err: any, data: any) {
+    fs.writeFileSync(assemblyFileParser.newFile, data, function (err: any) {
         if (err) {
             console.log('error: ' + err);
         } else {
