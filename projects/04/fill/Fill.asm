5ddf22242c19e8d436a270c19088b5f522593ac9
Steven Tuckness <stuckne1@gmail.com>
2019-08-30 16:00:38 -0500

    redo project 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,54 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+@fillbits
+M=0
+
+(LOOP)    
+    @SCREEN
+    D=A
+    @colorloopiterator
+    M=D
+    
+    @KBD 
+    D=M
+
+    @SETWHITEBITS
+    D;JEQ
+
+    @SETBLACKBITS
+    D;JNE
+
+(SETWHITEBITS)
+    @fillbits
+    M=0
+    @COLORFILL
+    0;JMP
+
+(SETBLACKBITS)
+    @fillbits
+    M=-1
+    @COLORFILL
+    0;JMP
+
+(COLORFILL)
+    @fillbits
+    D=M
+   
+    @colorloopiterator
+    A=M
+    M=D
+
+    @colorloopiterator
+    MD=M+1
+    
+    @24576
+    D=A-D
+
+    @COLORFILL
+    D;JNE
+ 
+    @LOOP
+    0;JMP
+
+   
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,38 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+@i          // i refers to some mem. location.
+M=1         // i=1
+@R2         // select R2 mem register
+M=0         // R2=0
+
+@R0
+D=M     // D=R0
+@END
+D;JEQ   // GOTO end
+
+@R1
+D=M     // D=R1
+@END
+D;JEQ   // GOTO end
+
+(LOOP)
+    @i
+    D=M     // D=i
+    @R1
+    D=D-M   // D=i-R1
+    @END
+    D;JGT   // If (i-R1) > 0 goto End
+
+    @R0
+    D=M     // D=R0
+    @R2     
+    M=D+M   // R2=R2+R0
+
+    @i
+    M=M+1   // i=i+1
+    @LOOP
+    0;JMP   // GOTO loop
+(END)
+    @END
+    0;JMP   // Infinite loop
